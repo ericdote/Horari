b8da2845c74d4082d2af276d5c194343d1c18c6a
ericdote <pokemon13>
2016-12-09 19:28:48 +0100

    Crea la BBDD y esta echo el select (hay que acabarlo 100%)

--- app/src/main/java/com/example/alumnedam/horari/MainActivity.java
+++ app/src/main/java/com/example/alumnedam/horari/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.alumnedam.horari;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Creamos la BBDD
+        SqlActivity sql = new SqlActivity(this, "Eric", null, 1);
+        //Creamos el objeto BBDD
+        SQLiteDatabase db = sql.getWritableDatabase();
+        //2 Strings, uno para la hora del sistema y otro para el grupo que escoja el usuario
+        String horaDelSistema;
+        String grupo = "A1";
+        //Cogemos la hora del sistema y le damos el formato que queremos.
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss");
+        horaDelSistema = format.format(calendar.getTime());
+        //Si la BBDD no esta vacia hace la SELECT
+        if(db != null){
+            String[] args = new String[] {horaDelSistema, grupo};
+            Cursor c = db.rawQuery(" SELECT * FROM tablaHorarios WHERE ? BETWEEN horaInici AND horaFi AND grup = ?", args);
+            if(c.moveToFirst()){
+                do{
+                    //TODO String http://www.sgoliver.net/blog/bases-de-datos-en-android-iii-consultarrecuperar-registros/
+                }while(c.moveToNext());
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/alumnedam/horari/SqlActivity.java
+++ app/src/main/java/com/example/alumnedam/horari/SqlActivity.java
@@ -10,9 +10,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class SqlActivity extends SQLiteOpenHelper {
     //Sentencias para crear las bases de datos
-    String tablaHorarios = "CREATE TABLE Horarios(Id_Horario INTEGER PRIMARY KEY, grup TEXT, codi_asignatura INTEGER, hora_inici TEXT, hora_fi TEXT, dia_setmana TEXT, profesor INTEGER, aula TEXT)";
-    String tablaProfesor = "CREATE TABLE Profesor(Id_profesor INTEGER PRIMARY KEY, Nom TEXT, codi_asignatura INTEGER)";
-    String tablaAsignatura = "CREATE TABLE Asignatura(Id_Asignatura INTEGER PRIMARY KEY, Nom TEXT, codi_asignatura TEXT)";
+    String tablaHorarios = "CREATE TABLE tablaHorarios(Id_Horario INTEGER PRIMARY KEY, grup TEXT, codi_asignatura INTEGER, hora_inici TEXT, hora_fi TEXT, dia_setmana TEXT, profesor INTEGER, aula TEXT)";
+    String tablaProfesor = "CREATE TABLE tablaProfesor(Id_profesor INTEGER PRIMARY KEY, Nom TEXT)";
+    String tablaAsignatura = "CREATE TABLE tablaAsignatura(Id_Asignatura INTEGER PRIMARY KEY, Nom TEXT, codi_asignatura TEXT, Profesor INTEGER)";
 
     public SqlActivity(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -30,15 +30,35 @@ public class SqlActivity extends SQLiteOpenHelper {
         db.execSQL(tablaAsignatura);
         //Insertamos las filas
 
-        db.execSQL("INSERT INTO Asignatura(1, Programació Orientada a Objectes, M03");
+        db.execSQL("INSERT INTO tablaProfesor VALUES(1, 'Josefa')");
+        db.execSQL("INSERT INTO tablaProfesor VALUES(2, 'Jose Antonio Leo')");
+        db.execSQL("INSERT INTO tablaProfesor VALUES(3, 'Jorge')");
+        db.execSQL("INSERT INTO tablaProfesor VALUES(4, 'LLuis')");
+        db.execSQL("INSERT INTO tablaProfesor VALUES(5, 'Marta')");
 
-        db.execSQL("INSERT INTO tablaHorarios VALUES (1,'Josefa', 'M07', '15:00', '18:00', 'Dilluns', 100, '208') ");
+        db.execSQL("INSERT INTO tablaAsignatura VALUES(1, 'Programació Orientada a Objectes', 'M03', 1)");
+        db.execSQL("INSERT INTO tablaAsignatura VALUES(2, 'Desenvolupament de interficies', 'M07', 2)");
+        db.execSQL("INSERT INTO tablaAsignatura VALUES(3, 'Programacio multimedia i dispositius mobils', 'M08', 4)");
+        db.execSQL("INSERT INTO tablaAsignatura VALUES(4, 'Programacio de serveis i processos', 'M09', 3)");
+        db.execSQL("INSERT INTO tablaAsignatura VALUES(5, 'Sistemes de gestió empresarial', 'M10', 5)");
+        db.execSQL("INSERT INTO tablaAsignatura VALUES(6, 'Pupurri de cosas con Jorge', 'M05/02/06', 3)");
+
+        db.execSQL("INSERT INTO tablaHorarios VALUES (1,'A1', '2', '15:00:00', '18:00:00', 'Dilluns', 2, '208') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (2,'A1', '1', '15:00:00', '17:00:00', 'Dimarts', '1', '208') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (3,'A1', '5', '17:00:00', '18:00:00', 'Dimarts', '5', '201') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (4,'Pati', 'Pati', '18:00', '18:20', 'Dimarts', null, 'Pati') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (5,'A1', '5', '18:20:00', '19:20:00', 'Dimarts', '5', '201') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (6,'A1', '6', '19:20:00', '21:20:00', 'Dimarts', '6', '201') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (7,'A1', '6', '16:00:00', '17:00:00', 'Dimecres', '6', '201') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (8,'A1', '4', '17:00:00', '18:00:00', 'Dimecres', '3', '201') ");
+        db.execSQL("INSERT INTO tablaHorarios VALUES (9,'Pati', 'Pati', '18:00', '18:20', 'Dimecres', null, 'Pati') ");
 
 
-        db.execSQL("INSERT INTO tablaHorarios VALUES (1,'A1', 'M07', '15:00', '18:00', 'Dilluns', 100, '208') ");
-        db.execSQL("INSERT INTO tablaHorarios VALUES (2,'A1', 'M03', '15:00', '17:00', 'Dimarts', 'Josefa', '208') ");
-        db.execSQL("INSERT INTO tablaHorarios VALUES (3,'A1', 'M10', '17:00', '18:00', 'Dimarts', 'Marta', '201') ");
-        db.execSQL("INSERT INTO tablaHorarios VALUES (4,'Pati', 'Pati', '18:00', '18:20', 'Dimarts', 'Marta', 'Pati') ");
+
+
+
+
+
 
 
     }
