b86910909636dcc1902a7f8852e237976089ca58
Eric <rovx15@gmail.com>
2016-12-31 15:37:26 +0100

    SharedPreferences OK. Grupo OK. Falta Dia...

--- app/src/main/java/com/example/alumnedam/horari/HorariActivity.java
+++ app/src/main/java/com/example/alumnedam/horari/HorariActivity.java
@@ -6,9 +6,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class HorariActivity extends AppCompatActivity {
 
@@ -19,52 +22,54 @@ public class HorariActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horari);
         String intentGrup = getIntent().getStringExtra("grup");
-        String idHorari, grup, codiAsignatura, horaInici, horaFi, diaSetmana, profesor, aula;
+        String grup, codiAsignatura, horaInici, horaFi, diaSetmana, diaSetmanaHorari, profesor, aula;
+        diaSetmana = diaDeLaSemana();
         //Creamos la BBDD
         SqlActivity sql = new SqlActivity(this, "Eric", null, 1);
         //Creamos el objeto BBDD
         db = sql.getWritableDatabase();
-        //2 Strings, uno para la hora del sistema y otro para el grupo que escoja el usuario
+        //String, para la hora del sistema
         String horaDelSistema;
         //Cogemos la hora del sistema y le damos el formato que queremos.
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat formato = new SimpleDateFormat("hh:mm:ss");
         horaDelSistema = formato.format(calendar.getTime());
         //Si la BBDD no esta vacia hace la SELECT
-        if(db != null){
-            String[] args = new String[] {horaDelSistema};
-            Cursor c = db.rawQuery("SELECT * FROM tablaHorarios WHERE ? BETWEEN hora_inici AND hora_fi", args);
-            if(c.moveToFirst()){
-                do{
-                    idHorari = c.getString(0);
+        if (db != null) {
+            String[] args = new String[]{horaDelSistema, intentGrup, diaSetmana};
+            Cursor c = db.rawQuery("SELECT * FROM tablaHorarios WHERE ? BETWEEN hora_inici AND hora_fi AND grup = ? AND dia_setmana = ?", args);
+            if (c.moveToFirst()) {
+                do {
                     grup = c.getString(1);
                     codiAsignatura = asignatura(c.getString(2));
                     horaInici = c.getString(3);
                     horaFi = c.getString(4);
-                    diaSetmana = c.getString(5);
+                    diaSetmanaHorari = c.getString(5);
                     profesor = profe(c.getString(6));
                     aula = c.getString(7);
-                }while(c.moveToNext());
+                } while (c.moveToNext());
                 //Llamamos a asignaTV para todos los valores anteriormente obtenidos colocarlos en TextViews
-                asignaTV(grup, codiAsignatura, horaInici, horaFi, diaSetmana, profesor, aula);
+                asignaTV(grup, codiAsignatura, horaInici, horaFi, diaSetmanaHorari, profesor, aula);
             }
         }
     }
 
+
     /**
      * Metodo que le llega por parametro la Id de una asignatura y se busca cual es su nombre.
      * Una vez obtenido el nombre se devuelve el resultado (nombre de la asignatura)
+     *
      * @param codiAsignatura
      * @return
      */
-    public String asignatura (String codiAsignatura){
-        String nom="";
-        String[] args = new String[] {codiAsignatura};
+    public String asignatura(String codiAsignatura) {
+        String nom = "";
+        String[] args = new String[]{codiAsignatura};
         Cursor c = db.rawQuery("SELECT Nom FROM tablaAsignatura WHERE ? LIKE Id_Asignatura", args);
-        if (c.moveToFirst()){
-            do{
+        if (c.moveToFirst()) {
+            do {
                 nom = c.getString(0);
-            } while(c.moveToNext());
+            } while (c.moveToNext());
         }
         return nom;
     }
@@ -72,45 +77,56 @@ public class HorariActivity extends AppCompatActivity {
     /**
      * Metodo que le llega por parametro la Id de un profesor y se busca cual es su nombre.
      * Una vez obtenido el nombre se devuelve el resultado (nombre del profesor)
+     *
      * @param profesor
      * @return
      */
-    public String profe (String profesor){
-        String nom ="";
-        String[] args = new String[] {profesor};
+    public String profe(String profesor) {
+        String nom = "";
+        String[] args = new String[]{profesor};
         Cursor c = db.rawQuery("SELECT Nom FROM tablaProfesor WHERE ? LIKE Id_profesor", args);
-        if (c.moveToFirst()){
-            do{
+        if (c.moveToFirst()) {
+            do {
                 nom = c.getString(0);
-            } while(c.moveToNext());
+            } while (c.moveToNext());
         }
         return nom;
     }
 
     /**
      * Metodo donde le llegan todos los String anteriores para poder asignar a TextViews los valores.
+     *
      * @param grup
      * @param codiAsignatura
      * @param horaInici
      * @param horaFi
-     * @param diaSetmana
+     * @param diaSetmanaHorari
      * @param profesor
      * @param aula
      */
-    public void asignaTV(String grup,String codiAsignatura,String horaInici,String horaFi,String diaSetmana,String profesor,String aula){
-        TextView hInici = (TextView)findViewById(R.id.tvHoraInAsignar);
+    public void asignaTV(String grup, String codiAsignatura, String horaInici, String horaFi, String diaSetmanaHorari, String profesor, String aula) {
+        TextView hInici = (TextView) findViewById(R.id.tvHoraInAsignar);
         hInici.setText(horaInici);
-        TextView hFi = (TextView)findViewById(R.id.tvHoraFinAsignar);
+        TextView hFi = (TextView) findViewById(R.id.tvHoraFinAsignar);
         hFi.setText(horaFi);
-        TextView dSetmana = (TextView)findViewById(R.id.tvDiaAsignar);
-        dSetmana.setText(diaSetmana);
-        TextView grupos = (TextView)findViewById(R.id.tvGrupAsignar);
+        TextView dSetmana = (TextView) findViewById(R.id.tvDiaAsignar);
+        dSetmana.setText(diaSetmanaHorari);
+        TextView grupos = (TextView) findViewById(R.id.tvGrupAsignar);
         grupos.setText(grup);
-        TextView clase = (TextView)findViewById(R.id.tvClaseAsignar);
+        TextView clase = (TextView) findViewById(R.id.tvClaseAsignar);
         clase.setText(aula);
-        TextView asig = (TextView)findViewById(R.id.tvAsignaturaAsignar);
+        TextView asig = (TextView) findViewById(R.id.tvAsignaturaAsignar);
         asig.setText(codiAsignatura);
-        TextView profe = (TextView)findViewById(R.id.tvProfesorAsignar);
+        TextView profe = (TextView) findViewById(R.id.tvProfesorAsignar);
         profe.setText(profesor);
+    }
+
+    public String diaDeLaSemana(){
+        String[] diesSetmana = new String[]{"Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"};
+        Calendar cal = Calendar.getInstance();
+        int dow = cal.get(Calendar.DAY_OF_WEEK);
+        String dia = diesSetmana[dow-5];
+        Toast.makeText(this, ""+dia, Toast.LENGTH_SHORT).show();
+        return dia;
     }
 }
--- app/src/main/java/com/example/alumnedam/horari/MainActivity.java
+++ app/src/main/java/com/example/alumnedam/horari/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.alumnedam.horari;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,22 +13,34 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.btnHorari);
+        Button btn = (Button) findViewById(R.id.btnHorari);
         btn.setOnClickListener(this);
 
     }
 
+    public void guardarPreferences(TextView tvNom, Spinner spin){
+        SharedPreferences prefs = getSharedPreferences("PreferenciasHorari", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("nom", tvNom.getText().toString());
+        editor.putString("grup", spin.getSelectedItem().toString());
+        editor.commit();
+    }
+
+
+
     @Override
     public void onClick(View view) {
-        TextView tvNom = (TextView)findViewById(R.id.etNom);
-        Spinner spin = (Spinner)findViewById(R.id.spinnerCurs);
+        TextView tvNom = (TextView) findViewById(R.id.etNom);
+        Spinner spin = (Spinner) findViewById(R.id.spinnerCurs);
         Intent intent = new Intent(this, HorariActivity.class);
-
-        if(!(tvNom.getText().toString().equals(""))){
+        if (!(tvNom.getText().toString().equals(""))) {
+            guardarPreferences(tvNom, spin);
             intent.putExtra("grup", spin.getSelectedItem().toString());
             startActivity(intent);
         } else {
